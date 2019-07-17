7861670a28a61dfe4bab5596c936fcb1a8505713
Jonathan Azali <azali.jonathan@gmail.com>
2019-07-17 07:52:46 -0700

    updated to mergeData

--- db.js
+++ db.js
@@ -16,12 +16,44 @@ ex after running delete('user.json'):
 Errors should also be logged (preferably in a human-readable format)
 */
 
+function log(value) {
+  return fs.appendFile('log.txt', `${value} ${Date.now()}\n`);
+}
+
 /**
  * Logs the value of object[key]
  * @param {string} file
  * @param {string} key
  */
-function get(file, key) {}
+
+async function get(file, key) {
+  /* Async/await approach */
+  try {
+    // 1. read file
+    // 2. handle promise -> data
+    const data = await fs.readFile(file, 'utf8');
+    // 3. parse data from string -> JSON
+    const parsed = JSON.parse(data);
+    // 4. use the key to get the value at object[key]
+    const value = parsed[key];
+    // 5. append the log file with the above value
+    if (!value) return log(`ERROR ${key} invalid key on ${file}`);
+    return log(value);
+  } catch (err) {
+    log(`ERROR no such file or directory ${file}`);
+  }
+  /* Promise-based approach
+  return fs
+    .readFile(file, 'utf8')
+    .then(data => {
+      const parsed = JSON.parse(data);
+      const value = parsed[key];
+      if (!value) return log(`ERROR ${key} invalid key on ${file}`);
+      return log(value);
+    })
+    .catch(err => log(`ERROR no such file or directory ${file}`));
+    */
+}
 
 /**
  * Sets the value of object[key] and rewrites object to file
@@ -29,28 +61,66 @@ function get(file, key) {}
  * @param {string} key
  * @param {string} value
  */
-function set(file, key, value) {}
+async function set(file, key, value) {
+  try {
+    // 1. Read File
+    const data = await fs.readFile(file, 'utf8');
+    // 2. Get Data
+    const parsed = JSON.parse(data);
+    // 3. Assign new Value to Key
+    parsed[key] = value;
+    // 4. Write to File Again
+    await fs.writeFile(file, JSON.stringify(parsed));
+  } catch (err) {
+    log(`ERROR no such file or directory ${file}`);
+  }
+}
 
 /**
  * Deletes key from object and rewrites object to file
  * @param {string} file
  * @param {string} key
  */
-function remove(file, key) {}
+async function remove(file, key) {
+  try {
+    // 1. Read File
+    const data = await fs.readFile(file, 'utf8');
+    // 2. Get Data
+    const parsed = JSON.parse(data);
+    // 3. Delete Key
+    delete parsed[key];
+    // 4. Write to File Again
+    await fs.writeFile(file, JSON.stringify(parsed));
+  } catch (err) {
+    log(`ERROR no such file or directory ${file}`);
+  }
+}
 
 /**
  * Deletes file.
  * Gracefully errors if the file does not exist.
  * @param {string} file
  */
-function deleteFile(file) {}
+async function deleteFile(file) {
+  try {
+    await fs.unlink(`${file}`);
+  } catch (err) {
+    log(`ERROR no such file or directory ${file}`);
+  }
+}
 
 /**
  * Creates file with an empty object inside.
  * Gracefully errors if the file already exists.
  * @param {string} file JSON filename
  */
-function createFile(file) {}
+async function createFile(file) {
+  try {
+    await fs.writeFile(`${file}`, '{}');
+  } catch (err) {
+    log(`ERROR no such file or directory ${file}`);
+  }
+}
 
 /**
  * Merges all data into a mega object and logs it.
@@ -70,7 +140,22 @@ function createFile(file) {}
  *    }
  * }
  */
-function mergeData() {}
+async function mergeData() {
+  try {
+    const megaObj = {};
+    const files = await fs.readdir('.');
+    const filteredFiles = files.filter(
+      file => file.includes('.json') && !file.includes('package')
+    );
+    for await (const file of filteredFiles) {
+      const trimmedFileName = file.slice(0, file.indexOf('.'));
+      megaObj[trimmedFileName] = JSON.parse(await fs.readFile(file, 'utf8'));
+    }
+    return log(JSON.stringify(megaObj));
+  } catch (err) {
+    return log(`ERROR ${err}`);
+  }
+}
 
 /**
  * Takes two files and logs all the properties as a list without duplicates
@@ -111,5 +196,5 @@ module.exports = {
   mergeData,
   union,
   intersect,
-  difference,
+  difference
 };
